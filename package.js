b02d9b0bb5c05fbab73f6e254f5382f44cf77a5a
Daniel Dornhardt <daniel@dornhardt.com>
2014-09-01 12:57:21 +0200

    where did the export go?!

--- package.js
+++ package.js
@@ -2,11 +2,12 @@ Package.describe({
   summary: "Simple log method for client and server.",
   homepage: "https://github.com/DanielDornhardt/yagni-log",
   author: "Daniel Dornhardt <daniel@dornhardt.com> (http://www.dornhardt.com)",
-  version: "1.0.2",
+  version: "1.0.3",
   git: "https://github.com/DanielDornhardt/yagni-log.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
+  api.export('log');
   api.addFiles('yagni:log.js');
 });
